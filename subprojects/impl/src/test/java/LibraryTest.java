c3655d7a67ebbc8b889db32f563cbe494c9b9797
Erhard Pointl <epeee@users.noreply.github.com>
2018-04-26 06:45:17 +0200

    minor change to trigger release (#188)

--- subprojects/impl/src/test/java/LibraryTest.java
+++ subprojects/impl/src/test/java/LibraryTest.java
@@ -9,6 +9,6 @@ public class LibraryTest {
     @Test
     public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        assertFalse("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
