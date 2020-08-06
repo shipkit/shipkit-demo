8e4c6ade4818c08df89b8f6e3335b4eb228094db
shipkit <shipkit.org@gmail.com>
2020-08-05 23:41:54 -0500

    Enabled GH releases

--- gradle/push-tag.sh
+++ gradle/push-tag.sh
@@ -2,14 +2,13 @@
 
 # Git push is implemented in the script to make sure we are not leaking GH key to the output
 # Expects:
-# 1. 'GIT_SECRET' env variable to be in format: user:github_access_token, for example: mockitoguy:qq43234xc23x23d24d
+# 1. 'GH_WRITE_TOKEN' env variable
 # 2. tag name as a parameter of this script
 
 TAG_NAME=$1
 
-echo "Running git push without output for security. If it fails make sure that GIT_SECRET env variable is set."
-echo "  The format of the secret should be user:github_access_token, for example: mockitoguy:qq43234xc23x23d24d"
-git push --quiet https://$GIT_SECRET@github.com/shipkit/shipkit-demo.git $TAG_NAME > /dev/null 2>&1
+echo "Running git push without output for security. If it fails make sure that GH_WRITE_TOKEN env variable is set."
+git push --quiet https://mockitoguy:$GH_WRITE_TOKEN@github.com/shipkit/shipkit-demo.git $TAG_NAME > /dev/null 2>&1
 EXIT_CODE=$?
 echo "'git push --quiet' exit code: $EXIT_CODE"
 exit $EXIT_CODE
